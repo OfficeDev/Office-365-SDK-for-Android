12baa40e5675ecc5fef89ba6bd3e01268fb148b8
Marcos Torres <marcost@lagash.com>
2014-09-18 18:20:17 -0300

    waypoint. app builds but has not been tested yet

--- samples/exchange-sample-olingo/src/main/java/com/example/exchangedemo/MainActivity.java
+++ samples/exchange-sample-olingo/src/main/java/com/example/exchangedemo/MainActivity.java
@@ -1,161 +1,210 @@
 package com.example.exchangedemo;
 
-import java.security.NoSuchAlgorithmException;
-
-import javax.crypto.NoSuchPaddingException;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.microsoft.aad.adal.AuthenticationCallback;
 import com.microsoft.aad.adal.AuthenticationCancelError;
 import com.microsoft.aad.adal.AuthenticationContext;
 import com.microsoft.aad.adal.AuthenticationResult;
+import com.microsoft.office365.api.MailClient;
+import com.microsoft.office365.oauth.OAuthCredentials;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import javax.crypto.NoSuchPaddingException;
 
 public class MainActivity extends Activity {
-	private static final String TAG = "MainActivity";
+    private static final String TAG = "MainActivity";
 
-	private static final int MENU_RESET_TOKEN = 0;
+    private static final int MENU_RESET_TOKEN = 0;
+    SimpleExchangeAdapter mailAdapter;
+    String mToken;
+    private AuthenticationContext mAuthContext;
+    private AppPreferences mAppPreferences;
+    private AndroidApplication mApplication;
 
-	private AuthenticationContext mAuthContext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	private AppPreferences mAppPreferences;
-	private AndroidApplication mApplication;
+        mApplication = (AndroidApplication) getApplication();
+        mAppPreferences = (mApplication).getAppPreferences();
 
-	TextView txtOutput;
-	TextView txtTokenResult;
-	ListView lvOutput;
-	Button btnGetAccessToken;
+        if (mApplication.hasConfiguration()) {
+            authenticate();
+        } else {
+            Intent intent = new Intent(MainActivity.this,
+                    AppPreferencesActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void authenticate() {
 
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+        getAuthContext().acquireToken(this, mAppPreferences.getResourceUrl(),
+                mAppPreferences.getClientId(),
+                mAppPreferences.getRedirectUrl(), "",
+                new AuthenticationCallback<AuthenticationResult>() {
 
-		txtTokenResult = (TextView) findViewById(R.id.textView_Token);
-		btnGetAccessToken = (Button) findViewById(R.id.button_getAccessToken);
-		txtOutput = (TextView) findViewById(R.id.textView_Output);
+                    @Override
+                    public void onError(Exception exc) {
+                        if (exc.getClass() == AuthenticationCancelError.class) {
+                            // User canceled
+                        } else {
+                            // Do something with the error
+                        }
+                    }
 
-		mApplication = (AndroidApplication) getApplication();
-		mAppPreferences = (mApplication).getAppPreferences();
+                    @Override
+                    public void onSuccess(AuthenticationResult result) {
+                        if (result != null
+                                && !result.getAccessToken().isEmpty()) {
+                            mToken = result.getAccessToken();
+                            getDataForListView();
+                        }
+                    }
+                });
+    }
 
-		btnGetAccessToken.setOnClickListener(new View.OnClickListener() {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mAuthContext.onActivityResult(requestCode, resultCode, data);
+    }
 
-			@Override
-			public void onClick(View v) {
+    protected AuthenticationContext getAuthContext() {
+        if (mAuthContext == null) {
+            try {
+                mAuthContext = new AuthenticationContext(this,
+                        Constants.AUTHORITY_URL, false);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (NoSuchPaddingException e) {
+                e.printStackTrace();
+            }
+        }
+        return mAuthContext;
+    }
 
-				if (mApplication.hasConfiguration()) {
-					getToken();
-				} else {
-					Intent intent = new Intent(MainActivity.this,
-							AppPreferencesActivity.class);
-					startActivity(intent);
-				}
-			}
-		});	
-	}
+    protected void getToken() {
+        getAuthContext().acquireToken(this, mAppPreferences.getResourceUrl(),
+                mAppPreferences.getClientId(),
+                mAppPreferences.getRedirectUrl(), Constants.USER_HINT,
+                new AuthenticationCallback<AuthenticationResult>() {
 
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		mAuthContext.onActivityResult(requestCode, resultCode, data);
-	}
+                    @Override
+                    public void onError(Exception exc) {
+                        if (exc.getClass() == AuthenticationCancelError.class) {
+                            Toast.makeText(getApplicationContext(), "User canceled",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), exc.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                            Log.e(TAG, exc.getMessage());
+                        }
+                    }
 
-	protected AuthenticationContext getAuthContext() {
-		if (mAuthContext == null) {
-			try {
-				mAuthContext = new AuthenticationContext(this,
-						Constants.AUTHORITY_URL, false);
-			} catch (NoSuchAlgorithmException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (NoSuchPaddingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return mAuthContext;
-	}
+                    @Override
+                    public void onSuccess(AuthenticationResult result) {
+                        if (result != null
+                                && !result.getAccessToken().isEmpty()) {
 
-	protected void getToken() {
-		//TODO: revisar segundo parametro
-		getAuthContext().acquireToken(this, mAppPreferences.getResourceUrl(),
-				mAppPreferences.getClientId(),
-				mAppPreferences.getRedirectUrl(), Constants.USER_HINT,
-				new AuthenticationCallback<AuthenticationResult>() {
+                            Toast.makeText(getApplicationContext(), "Got token!",
+                                    Toast.LENGTH_LONG).show();
+                            Log.i(TAG, result.getAccessToken());
+                        }
+                    }
+                });
+    }
 
-					@Override
-					public void onError(Exception exc) {
-						if(exc.getClass() == AuthenticationCancelError.class){
-							txtOutput.setText("User canceled");
-						}else
-						{
-							txtOutput.setText(exc.getMessage());						
-							Log.e(TAG, exc.getMessage());	
-						}
-						
-					}
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
 
-					@Override
-					public void onSuccess(AuthenticationResult result) {
-						if (result != null
-								&& !result.getAccessToken().isEmpty()) {
-							txtOutput.setText("Got token!");
-							txtTokenResult.setText(result.getAccessToken());
-							Log.i(TAG, result.getAccessToken());
-						}
-					}
-				});
-	}
+        menu.add(Menu.NONE, MENU_RESET_TOKEN, Menu.NONE, "Clear Token Cache");
+        return true;
+    }
 
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings: {
+                Intent intent = new Intent(MainActivity.this,
+                        AppPreferencesActivity.class);
+                startActivity(intent);
+                return true;
+            }
 
-		menu.add(Menu.NONE, MENU_RESET_TOKEN, Menu.NONE, "Clear Token Cache");
-		return true;
-	}
+            case R.id.new_mail: {
+                startActivity(new Intent(this, NewMailActivity.class));
+                return true;
+            }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.action_settings: {
-			Intent intent = new Intent(MainActivity.this,
-					AppPreferencesActivity.class);
-			startActivity(intent);
-			return true;
-		}
-		case MENU_RESET_TOKEN: {
-			resetToken();
-			clearCookies();
-			return true;
-		}
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
+            case R.id.load_mails: {
+                //getDataForListView();
+                return true;
+            }
 
-	public void resetToken() {
-		Log.i(TAG, "Clearing cached tokens");
-		getAuthContext().getCache().removeAll();
-		txtOutput.setText("");
-		txtTokenResult.setText("");
-	}
+            case MENU_RESET_TOKEN: {
+                resetToken();
+                clearCookies();
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-	public void clearCookies() {
-		CookieSyncManager.createInstance(getApplicationContext());
-		CookieManager cookieManager = CookieManager.getInstance();
-		cookieManager.removeAllCookie();
-		CookieSyncManager.getInstance().sync();
-	}
+    private void getDataForListView() {
+
+        MailClient client = new MailClient.Builder()
+                .setCredentials(new OAuthCredentials(mToken)).build();
+
+       /*                                   .build();
+        List<Message> messages = new ArrayList<Message>();
+        try {
+            messages = client.getMessages("Inbox").get();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        fillList(messages);
+        */
+
+    }
+
+    private void fillList(final List<MessageViewModel> messages) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mailAdapter = new SimpleExchangeAdapter(MainActivity.this, messages);
+                ListView mailListView = (ListView) findViewById(R.id.mail_list);
+                mailListView.setAdapter(mailAdapter);
+
+            }
+        });
+    }
+
+
+    public void resetToken() {
+        Log.i(TAG, "Clearing cached tokens");
+        getAuthContext().getCache().removeAll();
+    }
+
+    public void clearCookies() {
+        CookieSyncManager.createInstance(getApplicationContext());
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.removeAllCookie();
+        CookieSyncManager.getInstance().sync();
+    }
 
 }
