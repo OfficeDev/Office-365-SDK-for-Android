cdd81ca8a9d125d3a4ac3ca758802d1bd073cb46
Marcos Torres <marcost@lagash.com>
2014-09-18 16:42:33 -0300

    functional app with ADAL, wit no Olingo yet

---
+++ samples/exchange-sample-olingo/src/main/java/com/example/exchangedemo/AppPreferences.java
@@ -0,0 +1,27 @@
+package com.example.exchangedemo;
+
+import android.content.SharedPreferences;
+
+public class AppPreferences {
+
+	private SharedPreferences mPreferences;
+
+	public AppPreferences(SharedPreferences preferences) {
+		mPreferences = preferences;
+	}
+
+	public String getClientId() {
+		String clientId = mPreferences.getString("prefAADClientId", null);
+		return clientId;
+	}
+
+	public String getRedirectUrl() {
+		String redirectUrl = mPreferences.getString("prefAADRedirectUrl", null);
+		return redirectUrl;
+	}
+	
+	public String getResourceUrl() {
+		String url = mPreferences.getString("prefResourceUrl", null);
+		return url;
+	}
+}
