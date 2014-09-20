adbea5ddc5d67578b6824da41da3de83337cb228
Pablo Zaiden <pabloz@lagash.com>
2014-09-20 11:07:18 -0300

    Exchange sample with T4 autogen

---
+++ samples/exchange-t4-sample/app/src/main/java/org/example/exchanget4sample/MessageViewModel.java
@@ -0,0 +1,33 @@
+package org.example.exchanget4sample;
+
+public class MessageViewModel {
+
+    private String senderName;
+    private String subject;
+
+
+    public MessageViewModel(String senderName, String subject) {
+        this.senderName = senderName;
+        this.subject = subject;
+    }
+
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+
+    public String getSubject() {
+        return subject;
+    }
+
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
