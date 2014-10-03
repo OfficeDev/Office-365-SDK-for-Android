0344ee2327e5c06906cf8ec651e235f2962757e4
Pablo Zaiden <pabloz@lagash.com>
2014-10-03 18:30:51 -0300

    simple exchange sample

---
+++ samples/simple-exchange-sample/app/src/main/java/com/microsoft/simple_exchange_sample/Constants.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) Microsoft Open Technologies, Inc.
+ * All Rights Reserved
+ * See License.txt in the project root for license information. 
+ ******************************************************************************/
+package com.microsoft.simple_exchange_sample;
+
+public class Constants {
+
+	public static final String AUTHORITY_URL = "https://login.windows-ppe.net/common";
+	public static final String RESOURCE_ID = "https://sdfpilot.outlook.com";
+    public static final String REDIRECT_URL = "https://msopentechtest";
+    public static final String CLIENT_ID = "1216da67-c9b9-44fa-bdc4-d8e2a06b4c0f";
+	public static final String ENDPOINT_ID = RESOURCE_ID + "/ews/odata";
+	public static final String ENCRYPTION_KEY = "EncryptionKey";
+}
