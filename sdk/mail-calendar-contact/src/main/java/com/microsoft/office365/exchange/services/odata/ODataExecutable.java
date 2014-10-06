750420aa1af88eac35fac6a3be18f0e31c3622c3
Marcos Torres <marcost@lagash.com>
2014-10-06 18:03:46 -0300

    add initial param serialization code

--- sdk/mail-calendar-contact/src/main/java/com/microsoft/office365/exchange/services/odata/ODataExecutable.java
+++ sdk/mail-calendar-contact/src/main/java/com/microsoft/office365/exchange/services/odata/ODataExecutable.java
@@ -9,6 +9,7 @@ import com.google.common.util.concurrent.ListenableFuture;
 import com.microsoft.office365.odata.interfaces.*;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.HashMap;
 
 abstract class ODataExecutable {
 
@@ -16,7 +17,14 @@ abstract class ODataExecutable {
 
     abstract DependencyResolver getResolver();
 
-	protected String urlEncode(String s){
+    String getODataParameterValue(Object value){
+
+        String serialized = getResolver().getJsonSerializer().serialize(value);
+        return urlEncode(serialized);
+    }
+
+
+    protected String urlEncode(String s){
         try {
             return URLEncoder.encode(s, Constants.UTF8_NAME);
         } catch (UnsupportedEncodingException ignore) {
