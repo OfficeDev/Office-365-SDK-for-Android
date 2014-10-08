03e3c54513912c6995d04e3dce64a775f5cb220a
anihojnadel <anahi.hojnadel@gmail.com>
2014-10-08 14:59:18 -0300

    Missing Imported modules

---
+++ tests/Office365-files-lists-sdk-e2etests-gradle/mail-calendar-contact/src/main/java/com/microsoft/office365/exchange/services/model/PatternedRecurrence.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) Microsoft Open Technologies, Inc.
+ * All Rights Reserved
+ * See License.txt in the project root for license information.
+ ******************************************************************************/
+package com.microsoft.office365.exchange.services.model;
+
+public class PatternedRecurrence {
+
+	private RecurrencePattern pattern;
+
+	public RecurrencePattern getPattern() {
+		 return pattern; 
+	}
+
+	public void setPattern(RecurrencePattern value) { 
+		pattern = value; 
+	}
+
+	private RecurrenceRange range;
+
+	public RecurrenceRange getRange() {
+		 return range; 
+	}
+
+	public void setRange(RecurrenceRange value) { 
+		range = value; 
+	}
+}
---
+++ tests/Office365-files-lists-sdk-e2etests-gradle/mail-calendar-contact/src/main/java/com/microsoft/office365/exchange/services/odata/MessageOperations.java
@@ -0,0 +1,251 @@
+/*******************************************************************************
+ * Copyright (c) Microsoft Open Technologies, Inc.
+ * All Rights Reserved
+ * See License.txt in the project root for license information.
+ ******************************************************************************/
+package com.microsoft.office365.exchange.services.odata;
+
+import com.google.common.util.concurrent.*;
+import com.microsoft.office365.odata.interfaces.*;
+import com.microsoft.office365.exchange.services.model.*;
+
+public class MessageOperations extends ODataOperations {
+
+	 public MessageOperations(String urlComponent, ODataExecutable parent) {
+        super(urlComponent, parent);
+    }
+			
+	public ListenableFuture<Message> copy(String destinationId) {
+        final SettableFuture<Message> result = SettableFuture.create();
+
+        ListenableFuture<byte[]> future = oDataExecute("Copy", null, HttpVerb.POST);
+
+        Futures.addCallback(future, new FutureCallback<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                DependencyResolver resolver = getResolver();
+
+                try {
+                    result.set(resolver.getJsonSerializer().deserialize(new String(bytes, Constants.UTF8), Message.class));
+                } catch (Throwable throwable) {
+                    result.setException(throwable);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                result.setException(t);
+            }
+        });
+
+        return result;
+    }
+			
+	public ListenableFuture<Message> move(String destinationId) {
+        final SettableFuture<Message> result = SettableFuture.create();
+
+        ListenableFuture<byte[]> future = oDataExecute("Move", null, HttpVerb.POST);
+
+        Futures.addCallback(future, new FutureCallback<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                DependencyResolver resolver = getResolver();
+
+                try {
+                    result.set(resolver.getJsonSerializer().deserialize(new String(bytes, Constants.UTF8), Message.class));
+                } catch (Throwable throwable) {
+                    result.setException(throwable);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                result.setException(t);
+            }
+        });
+
+        return result;
+    }
+			
+	public ListenableFuture<Message> createReply() {
+        final SettableFuture<Message> result = SettableFuture.create();
+
+        ListenableFuture<byte[]> future = oDataExecute("CreateReply", null, HttpVerb.POST);
+
+        Futures.addCallback(future, new FutureCallback<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                DependencyResolver resolver = getResolver();
+
+                try {
+                    result.set(resolver.getJsonSerializer().deserialize(new String(bytes, Constants.UTF8), Message.class));
+                } catch (Throwable throwable) {
+                    result.setException(throwable);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                result.setException(t);
+            }
+        });
+
+        return result;
+    }
+			
+	public ListenableFuture<Message> createReplyAll() {
+        final SettableFuture<Message> result = SettableFuture.create();
+
+        ListenableFuture<byte[]> future = oDataExecute("CreateReplyAll", null, HttpVerb.POST);
+
+        Futures.addCallback(future, new FutureCallback<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                DependencyResolver resolver = getResolver();
+
+                try {
+                    result.set(resolver.getJsonSerializer().deserialize(new String(bytes, Constants.UTF8), Message.class));
+                } catch (Throwable throwable) {
+                    result.setException(throwable);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                result.setException(t);
+            }
+        });
+
+        return result;
+    }
+			
+	public ListenableFuture<Message> createForward() {
+        final SettableFuture<Message> result = SettableFuture.create();
+
+        ListenableFuture<byte[]> future = oDataExecute("CreateForward", null, HttpVerb.POST);
+
+        Futures.addCallback(future, new FutureCallback<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                DependencyResolver resolver = getResolver();
+
+                try {
+                    result.set(resolver.getJsonSerializer().deserialize(new String(bytes, Constants.UTF8), Message.class));
+                } catch (Throwable throwable) {
+                    result.setException(throwable);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                result.setException(t);
+            }
+        });
+
+        return result;
+    }
+			
+	public ListenableFuture<Integer> reply(String comment) {
+        final SettableFuture<Integer> result = SettableFuture.create();
+
+        ListenableFuture<byte[]> future = oDataExecute("Reply", null, HttpVerb.POST);
+
+        Futures.addCallback(future, new FutureCallback<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                DependencyResolver resolver = getResolver();
+
+                try {
+                    result.set(resolver.getJsonSerializer().deserialize(new String(bytes, Constants.UTF8), Integer.class));
+                } catch (Throwable throwable) {
+                    result.setException(throwable);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                result.setException(t);
+            }
+        });
+
+        return result;
+    }
+			
+	public ListenableFuture<Integer> replyAll(String comment) {
+        final SettableFuture<Integer> result = SettableFuture.create();
+
+        ListenableFuture<byte[]> future = oDataExecute("ReplyAll", null, HttpVerb.POST);
+
+        Futures.addCallback(future, new FutureCallback<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                DependencyResolver resolver = getResolver();
+
+                try {
+                    result.set(resolver.getJsonSerializer().deserialize(new String(bytes, Constants.UTF8), Integer.class));
+                } catch (Throwable throwable) {
+                    result.setException(throwable);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                result.setException(t);
+            }
+        });
+
+        return result;
+    }
+			
+	public ListenableFuture<Integer> forward(String comment, java.util.List<Recipient> toRecipients) {
+        final SettableFuture<Integer> result = SettableFuture.create();
+
+        ListenableFuture<byte[]> future = oDataExecute("Forward", null, HttpVerb.POST);
+
+        Futures.addCallback(future, new FutureCallback<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                DependencyResolver resolver = getResolver();
+
+                try {
+                    result.set(resolver.getJsonSerializer().deserialize(new String(bytes, Constants.UTF8), Integer.class));
+                } catch (Throwable throwable) {
+                    result.setException(throwable);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                result.setException(t);
+            }
+        });
+
+        return result;
+    }
+			
+	public ListenableFuture<Integer> send() {
+        final SettableFuture<Integer> result = SettableFuture.create();
+
+        ListenableFuture<byte[]> future = oDataExecute("Send", null, HttpVerb.POST);
+
+        Futures.addCallback(future, new FutureCallback<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                DependencyResolver resolver = getResolver();
+
+                try {
+                    result.set(resolver.getJsonSerializer().deserialize(new String(bytes, Constants.UTF8), Integer.class));
+                } catch (Throwable throwable) {
+                    result.setException(throwable);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                result.setException(t);
+            }
+        });
+
+        return result;
+    }
+}
