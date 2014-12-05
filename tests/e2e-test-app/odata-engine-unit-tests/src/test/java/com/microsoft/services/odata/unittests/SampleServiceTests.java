5ccaca16b264c9a217977d00927454cd08ae4e20
Marcos Torres <marcost@lagash.com>
2014-12-05 12:42:11 -0300

    adding initial test code sample services

---
+++ tests/e2e-test-app/odata-engine-unit-tests/src/test/java/com/microsoft/services/odata/unittests/SampleServiceTests.java
@@ -0,0 +1,16 @@
+package com.microsoft.services.odata.unittests;
+
+
+import com.microsoft.sampleservice.SampleContainerClient;
+
+import org.junit.Test;
+
+public class SampleServiceTests extends WireMockTestBase {
+
+    private String url = "";
+
+    @Test
+    public void queryTwoParamsActionsFirstIsEntityType(){
+
+    }
+}
