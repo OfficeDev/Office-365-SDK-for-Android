554e9301f4bfddbf285d750ef81e969b6fe41785
Anahi Hojnadel <anahi.hojnadel@gmail.com>
2015-01-02 17:17:28 -0300

    Changed test order for testing purposes

--- tests/unit-test/odata-engine-unit-tests/src/test/java/com/microsoft/services/odata/unittests/SampleServiceTests.java
+++ tests/unit-test/odata-engine-unit-tests/src/test/java/com/microsoft/services/odata/unittests/SampleServiceTests.java
@@ -280,26 +280,6 @@ public class SampleServiceTests extends WireMockTestBase {
         assertThat(result, is(equalTo(responseContent)));
     }
 
-    @Ignore
-    @Test
-    public void testGetCollectionWithFilterAndExpand() throws ExecutionException, InterruptedException {
-        //getCollectionsWithExpandAndFilter.json
-        List<SampleEntity> result = null;
-
-        try {
-            result = client.getservices()
-                    .expand("SomeProp")
-                    .filter("SomeProp eq 'SomeString'")
-                    .read()
-                    .get();
-        } catch (Throwable t) {
-            resolver.getLogger().log(t.getLocalizedMessage(), LogLevel.ERROR);
-        }
-
-        assertThat(result, is(notNullValue()));
-        assertThat(result.size(), is(equalTo(1)));
-    }
-
     @Test
     public void testGetCollectionWithHeaders() throws ExecutionException, InterruptedException {
         //getCollectionsWithHeaders.json
@@ -413,6 +393,25 @@ public class SampleServiceTests extends WireMockTestBase {
         }
 
         assertThat(result, is(notNullValue()));
+    }
+
+    @Test
+    public void testGetCollectionWithFilterAndExpand() throws ExecutionException, InterruptedException {
+        //getCollectionsWithExpandAndFilter.json
+        List<SampleEntity> result = null;
+
+        try {
+            result = client.getservices()
+                    .expand("SomeProp")
+                    .filter("SomeProp eq 'SomeString'")
+                    .read()
+                    .get();
+        } catch (Throwable t) {
+            resolver.getLogger().log(t.getLocalizedMessage(), LogLevel.ERROR);
+        }
+
+        assertThat(result, is(notNullValue()));
+        assertThat(result.size(), is(equalTo(1)));
     }
 
     private SampleEntity getSampleEntity(){
