b16ef3e9106f444467cd6f7e6f4b558545de5973
Anahi Hojnadel <anahi.hojnadel@gmail.com>
2015-12-02 23:44:31 -0300

    Added Events to MyGraphApp sample

--- samples/MyGraphApp/app/src/main/java/com/microsoft/mygraphapp/MainActivity.java
+++ samples/MyGraphApp/app/src/main/java/com/microsoft/mygraphapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -192,6 +194,24 @@ public class MainActivity extends AppCompatActivity {
         mAuthContext.onActivityResult(requestCode, resultCode, data);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.events:
+                startActivity(new Intent(this, EventsActivity.class));
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     protected void loadInformation(){
         loadProfilePhoto();
